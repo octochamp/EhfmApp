97b1af16cd7301348737c14077e0b88c980c0dd1
Nathan <49677546+octochamp@users.noreply.github.com>
2025-04-16 16:42:40 +0100

    Bumped version number to 1.0.0

--- currentVersion.js
+++ currentVersion.js
@@ -1,5 +1,5 @@
 const currentVersion = () => {
-  const version = "0.1.8";
+  const version = "1.0.0";
   const buildDate = "2025 04 16";
   // set releaseType to 'release' to hide Beta modal on launch
   const releaseType = "beta";
