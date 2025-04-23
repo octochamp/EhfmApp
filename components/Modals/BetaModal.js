19fd9bab5a5a336b2b5856affd8b3053f57858b0
octochamp <49677546+octochamp@users.noreply.github.com>
2025-04-23 14:48:12 +0100

    Updated beta welcome info.

--- components/Modals/BetaModal.js
+++ components/Modals/BetaModal.js
@@ -20,9 +20,9 @@ const BetaModal = ({ isVisible, onClose }) => {
                     </Pressable>
                     <ScrollView fadingEdgeLength={150}>
                         <Text style={styles.modalH1}>Welcome! 👋</Text>
-                        <Text style={styles.modalH2}>You're beta testing the app!</Text>
-                        <Text style={styles.modalBody}>Thanks for taking the time to try it out. You might come across bugs or find that features are missing.</Text>
-                        <Text style={styles.modalBody}>If you find a bug or have a suggestion, please let us know through TestFlight (on iOS) by shaking your device, or if you're on Android leave a review on the Play Store. You can also get in touch or help out with app development on Github through the Report option in the menu.</Text>
+                        <Text style={styles.modalH2}>You're beta testing the new EHFM app!</Text>
+                        <Text style={styles.modalBody}>Thanks for helping out! You might come across bugs or find that features are missing.</Text>
+                        <Text style={styles.modalBody}>If you find a bug or have a suggestion, please let us know through TestFlight (on iOS) by shaking your device, or if you're on Android leave a review on the Play Store. You can also get in touch through the Report option in the menu.</Text>
                         <Text style={styles.modalSub}>EhfmApp v{currentVersion().version} {`(${currentVersion().releaseType})`}, built {currentVersion().buildDate}</Text>
                     </ScrollView>
                 </View>
